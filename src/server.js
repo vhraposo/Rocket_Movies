f462a2e9bc52b1a93d250bc45ad03c50ffb1a882
Victor Raposo <thevhup@gmail.com>
2023-09-04 15:31:37 -0300

    create user sign route

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app = express()
+
+app.get("/message/:id/:user", (request, response) => {
+  const { id, user } = request.params
+
+
+  response.send(`Id da mensagem: ${id}.
+  Para o usuário: ${user}`)
+})
+
+app.get("/users", (request, response) => {
+  const { page, limit } = request.query
+
+  response.send(`Página: ${page}. Mostrar: ${limit}`)
+})
+
+const PORT = 3333
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
