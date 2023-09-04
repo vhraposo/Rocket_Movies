134e4af2e49d31e197cc7beb39e2e0e821bcf449
Victor Raposo <thevhup@gmail.com>
2023-09-04 16:52:23 -0300

    feat: add users controller

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,14 +1,12 @@
 const { Router } = require("express")
 
+const UsersController = require("../controllers/UsersController")
+
+
 const usersRoutes = Router()
 
+const usersController = new UsersController()
 
-usersRoutes.post("/", (request, response) => {
-  const { name, email, password } = request.body
-
-  response.json({ name, email, password})
-
-
-})
+usersRoutes.post("/", usersController.create)
 
 module.exports = usersRoutes
