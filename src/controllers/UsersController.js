66f9b4fa80e7a79a2119328148c80f0c9908f9c1
vhraposo <thevhup@gmail.com>
2023-09-08 09:18:53 -0300

    feat add bcrypt password at userscontroller

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,3 +1,4 @@
+const { hash } = require("bcryptjs")
 const AppError = require("../utils/AppError")
 
 const sqliteConnection = require("../database/sqlite")
@@ -13,8 +14,12 @@ class UsersController{
     if(checkUserExist){
       throw new AppError("Este e-mail já está em uso.")
     }
+
+    const hashedPassword = await hash(password, 8)
+
+
     await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-    [name, email, password])
+    [name, email, hashedPassword])
 
 
     return response.status(201).json()
