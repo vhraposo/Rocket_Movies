8de769bb97695e4958a1c8e3a0df8e15de7f840d
vhraposo <thevhup@gmail.com>
2023-09-19 19:30:30 -0300

    feat: add delete movie at MoviesNotesControllers

--- src/controllers/MoviesNotesController.js
+++ src/controllers/MoviesNotesController.js
@@ -36,6 +36,14 @@ class MoviesNotesController{
         movieTags
       })
     }
+
+    async delete(request, response){
+      const { id } = request.params
+
+      await knex("movies_notes").where({ id }).delete()
+
+      return response.json()
+    }
 }
 
 module.exports = MoviesNotesController
--- src/routes/movies_notes.routes.js
+++ src/routes/movies_notes.routes.js
@@ -9,6 +9,8 @@ const moviesNotesController = new MoviesNotesController()
 
 moviesNotesRoutes.post('/:user_id', moviesNotesController.create)
 moviesNotesRoutes.get('/:id', moviesNotesController.show)
+moviesNotesRoutes.delete('/:id', moviesNotesController.delete)
+
 
 
 
